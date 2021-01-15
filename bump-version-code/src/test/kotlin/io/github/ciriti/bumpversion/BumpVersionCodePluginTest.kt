d20457b8ca497ef6592b17c30951526df988bb4f
Sourcepoint <ciriti@gmail.com>
2021-01-15 12:38:30 +0100

    added bump plugin

---
+++ bump-version-code/src/test/kotlin/io/github/ciriti/bumpversion/BumpVersionCodePluginTest.kt
@@ -0,0 +1,69 @@
+package io.github.ciriti.bumpversion
+
+import io.github.ciriti.bumpversion.Constants.TASK_NAME
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+
+class BumpVersionCodePluginTest {
+
+    @Rule
+    @JvmField
+    val testProjectDir: TemporaryFolder = TemporaryFolder()
+
+    private val buildFile: File by lazy {
+        testProjectDir.newFile("build.gradle")
+    }
+
+    private val gradleRunner: GradleRunner by lazy {
+        GradleRunner.create()
+            .withDebug(true)
+            .withPluginClasspath()
+            .withProjectDir(testProjectDir.root)
+            .withTestKitDir(testProjectDir.newFolder())
+    }
+
+    @Before
+    fun setup() {
+    }
+
+    @Test
+    fun `GIVEN a gradle properties file into app INCREASE the VERSION_CODE`() {
+        buildFile.appendText("build.gradle.txt".readFileContent())
+
+        testProjectDir.run {
+            newFolder("app")
+            newFile("app/gradle.properties")
+                .appendText("gradle.properties".readFileContent())
+        }
+
+        val res = gradleRunner
+            .withArguments(TASK_NAME)
+            .build()
+
+        Assert.assertTrue(res.output.contains("VERSION_CODE=2"))
+        Assert.assertEquals(TaskOutcome.SUCCESS, res.task(":$TASK_NAME")!!.outcome)
+    }
+
+    @Test
+    fun `GIVEN a gradle properties file into the root project INCREASE the VERSION_CODE`() {
+        buildFile.appendText("build.gradle.ext.txt".readFileContent())
+
+        testProjectDir.run {
+            newFile("gradle.properties")
+                .appendText("gradle.properties".readFileContent())
+        }
+
+        val res = gradleRunner
+            .withArguments(TASK_NAME)
+            .build()
+
+        Assert.assertTrue(res.output.contains("VERSION_CODE=2"))
+        Assert.assertEquals(TaskOutcome.SUCCESS, res.task(":$TASK_NAME")!!.outcome)
+    }
+}
